221e8c8e6f53f24cf76c5f3edcbac4cd8bcb98c4
try <orgelb06@gmail.com>
2024-09-18 14:24:25 +0300

    first Data Handling commit

---
+++ Data_Handling.py
@@ -0,0 +1,30 @@
+import pandas
+import tabula
+from tabula import read_pdf
+
+jerosalem_shelters = pandas.read_csv("EN_Shelters.csv", encoding="ISO-8859-1")
+jerosalem_shelters = jerosalem_shelters.dropna()
+print(jerosalem_shelters[["Address","Area","Capacity"]])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # jerosalem_shelters = jerosalem_shelters.drop("מספר מקלט", axis= 1)
+# print(f"Found {len(jerosalem_shelters)} tables")
+# print(jerosalem_shelters)
+# print(jerosalem_shelters.head(8))
+# # count = 1
+# # for shelters in jerosalem_shelters:
+# #     print(f"Area {count}")
+# #     print(shelters)
+# #     count += 1
